3009fcf8a1cb707d1acce8c625614d5a406a0385
discolingua <discolingua@gmail.com>
2013-08-24 10:00:31 -0400

    basic movement

---
+++ src/entities/Player.hx
@@ -0,0 +1,105 @@
+package entities;
+
+import com.haxepunk.Entity;
+import com.haxepunk.HXP;
+import com.haxepunk.graphics.Image;
+import com.haxepunk.utils.Input;
+import com.haxepunk.utils.Key;
+
+class Player extends Entity
+{
+
+    private var xVel:Float;
+    private var yVel:Float;
+    private var xAcceleration:Float;
+    private var yAcceleration:Float;
+
+    private static inline var maxVelocity:Float = 8;
+    private static inline var speed:Float = 3;
+    private static inline var drag:Float = 0.4;
+
+    public function new(x:Int, y:Int)
+    {
+	super(x, y);
+
+	graphic = new Image ("gfx/block.png");
+	setHitbox(32,32);
+
+	Input.define("up", [Key.UP, Key.W]);
+	Input.define("down", [Key.DOWN, Key.S]);
+	Input.define("left", [Key.LEFT, Key.A]);
+	Input.define("right", [Key.RIGHT, Key.D]);
+
+	xVel = 0;
+	yVel = 0;
+
+	type = "player";
+    }
+
+    public override function update()
+    {
+	handleInput();
+	move();
+	moveBy(xVel,yVel);
+
+	super.update();
+    }
+
+    private function handleInput()
+    {
+	xAcceleration = 0;
+	yAcceleration = 0;
+
+	if (Input.check("left"))
+	    {
+		xAcceleration = -1;
+	    }
+	if (Input.check("right"))
+	    {
+		xAcceleration = 1;
+	    }
+	if (Input.check("up"))
+	    {
+		yAcceleration = -1;
+	    }
+	if (Input.check("down"))
+	    {
+		yAcceleration = 1;
+	    }
+    }
+
+    private function move()
+    {
+	xVel += xAcceleration * speed;
+	yVel += yAcceleration * speed;
+
+	if(Math.abs(xVel) > maxVelocity)
+	    {
+		xVel = maxVelocity * HXP.sign(xVel);
+	    }
+
+	if(xVel < 0)
+	    {
+		xVel = Math.min(xVel + drag, 0);
+	    }
+	else if (xVel > 0)
+	    {
+		xVel = Math.max(xVel - drag, 0);
+	    }
+
+	if(Math.abs(yVel) > maxVelocity)
+	    {
+		yVel = maxVelocity * HXP.sign(yVel);
+	    }
+
+	if(yVel < 0)
+	    {
+		yVel = Math.min(yVel + drag, 0);
+	    }
+	else if (yVel > 0)
+	    {
+		yVel = Math.max(yVel - drag, 0);
+	    }
+    }
+}
+
